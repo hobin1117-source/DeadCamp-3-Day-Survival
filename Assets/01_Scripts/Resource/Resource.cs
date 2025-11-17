7de4f017d06406a6699ef7df222cc6b18d749900
qkrtkdgus010208 <psh010208@naver.com>
2025-11-17 12:14:30 +0900

    feat: 자원 리스폰 구현

--- Assets/01_Scripts/Resource/Resource.cs
+++ Assets/01_Scripts/Resource/Resource.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
@@ -6,14 +6,30 @@ public class Resource : MonoBehaviour
     public int quantityPerHit = 1;
     public int capacity;
 
+    private ResourceSpawner resourceSpawner;
+
+    private void Start()
+    {
+        resourceSpawner = Managers.Resource;
+    }
+
     public void Gathering(Vector3 hitPoint, Vector3 hitNormal)
     {
         for (int i = 0; i < quantityPerHit; i++)
         {
-            if (capacity <= 0) break;
+            if (capacity <= 0)
+            {
+                Release();
+                break;
+            }
             capacity -= 1;
             Instantiate(itemToGive.dropPrefabs, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
 
         }
+    }
+
+    private void Release()
+    {
+        resourceSpawner.InsertQueue(gameObject);
     }
 }
---
+++ Assets/01_Scripts/Resource/ResourceSpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ResourceSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnRange;
+    [SerializeField] private BoxCollider rangeCollider;
+    [SerializeField] private GameObject[] resources;
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    [SerializeField] private int initialSize = 30;
+    [SerializeField] private float spawnDleay = 30;
+
+    public void Start()
+    {
+        for (int i = 0; i < initialSize; i++)
+        {
+            GameObject resource = resources[Random.Range(0, resources.Length)];
+            GameObject go = Instantiate(resource, gameObject.transform);
+            go.transform.Rotate(new Vector3(0, Random.Range(-180, 180), 0));
+            pool.Enqueue(go);
+            go.SetActive(false);
+        }
+
+        StartCoroutine(Respawn());
+    }
+
+    public void InsertQueue(GameObject go)
+    {
+        pool.Enqueue(go);
+        go.SetActive(false);
+    }
+
+    public GameObject GetQueue()
+    {
+        GameObject go = pool.Dequeue();
+        go.SetActive(true);
+
+        return go;
+    }
+
+    IEnumerator Respawn()
+    {
+        float dealy = 0.01f;
+        while (true)
+        {
+            if (pool.Count != 0)
+            {
+                GameObject go = GetQueue();
+                go.transform.position = GetRandomPosition();
+                if (pool.Count <= 5)
+                    dealy = spawnDleay;
+            }
+            yield return new WaitForSeconds(dealy);
+        }
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        Vector3 originPosition = spawnRange.transform.position;
+  
+        float rangeX = rangeCollider.bounds.size.x;
+        float rangeZ = rangeCollider.bounds.size.z;
+
+        rangeX = Random.Range((rangeX / 2) * -1, rangeX / 2);
+        rangeZ = Random.Range((rangeZ / 2) * -1, rangeZ / 2);
+        Vector3 RandomPostion = new Vector3(rangeX, 0f, rangeZ);
+
+        Vector3 respawnPosition = originPosition + RandomPostion;
+        return respawnPosition;
+    }
+}
