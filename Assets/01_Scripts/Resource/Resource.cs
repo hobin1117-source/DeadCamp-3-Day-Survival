46171b8275ce447fd2ea614f01d8211193bd807a
qkrtkdgus010208 <psh010208@naver.com>
2025-11-18 17:12:40 +0900

    fix: 프리팹 셋팅 오류 수정

--- Assets/01_Scripts/Resource/Resource.cs
+++ Assets/01_Scripts/Resource/Resource.cs
@@ -4,7 +4,7 @@ public class Resource : MonoBehaviour
 {
     [SerializeField] private ItemData itemToGive;
     [SerializeField] private int quantityPerHit = 1;
-    [SerializeField] private int maxCapacity;
+    [SerializeField] private int MaxCapacity;
     [SerializeField] private int curCapacity;
 
     public int spawnIndex;
@@ -14,7 +14,7 @@ public class Resource : MonoBehaviour
     public void Init(ResourceSpawner resourceSpawner)
     {
         this.resourceSpawner = resourceSpawner;
-        curCapacity = maxCapacity;
+        curCapacity = MaxCapacity;
     }
 
     public void Gathering(Vector3 hitPoint, Vector3 hitNormal)
